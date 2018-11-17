eb704252d39e9c19cf1688a5663d440c9697f401
William A. Kennington III <wak@google.com>
2018-11-16 19:57:00 -0800

    File Descriptor Support
    Change-Id: I55b9df9ee81109bac5ecfb66a8b53bb5db648de8

---
+++ src/stdplus/fd/base.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <stdplus/internal/sys.hpp>
+
+namespace stdplus
+{
+namespace fd
+{
+
+/** @brief An interface to retrieve a stored file descriptor
+ *         and underlying libc implementation.
+ */
+class Fd
+{
+  public:
+    virtual ~Fd() = default;
+
+    /** @brief Gets the integer represting the file descriptor
+     *  @return The file descriptor number.
+     */
+    virtual int getVal() const = 0;
+
+    /** @brief Gets the interface used for making calls on the descriptor
+     *  @return The interface.
+     */
+    virtual const internal::Sys* getSys() const noexcept = 0;
+};
+
+} // namespace fd
+} // namespace stdplus
