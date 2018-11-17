d52ec1c8e364aba8ac9a0a4e780d93e2a1b1f005
William A. Kennington III <wak@google.com>
2018-11-16 19:57:00 -0800

    fd: Add support for managed file descriptors
    This change adds support for unowned file descriptor references,
    uniquely owned file descriptors, and copyable file descriptors. These
    are useful because they convey the lifetimes and ownership of a file
    descriptor and correctly close the descriptor using RAII.
    
    Change-Id: I55b9df9ee81109bac5ecfb66a8b53bb5db648de8
    Signed-off-by: William A. Kennington III <wak@google.com>

---
+++ src/stdplus/internal/sys.cpp
@@ -0,0 +1,28 @@
+#include <fcntl.h>
+#include <stdplus/internal/sys.hpp>
+#include <unistd.h>
+
+namespace stdplus
+{
+namespace internal
+{
+
+int SysImpl::open(const char* pathname, int flags) const
+{
+    return ::open(pathname, flags);
+}
+
+int SysImpl::dup(int oldfd) const
+{
+    return ::dup(oldfd);
+}
+
+int SysImpl::close(int fd) const
+{
+    return ::close(fd);
+}
+
+SysImpl sys_impl;
+
+} // namespace internal
+} // namespace stdplus
---
+++ test/fd/dupable.cpp
@@ -0,0 +1,78 @@
+#include <gtest/gtest.h>
+#include <stdplus/fd/dupable.hpp>
+#include <stdplus/test/sys.hpp>
+
+namespace stdplus
+{
+namespace fd
+{
+namespace
+{
+
+using testing::Return;
+
+TEST(DupableFd, BasicMoveConstruct)
+{
+    testing::StrictMock<test::SysMock> mock;
+    DupableFd fd(1, &mock);
+    EXPECT_EQ(1, fd.getVal());
+    EXPECT_CALL(mock, close(1)).WillOnce(Return(0));
+}
+
+TEST(DupableFd, BasicCopyConstruct)
+{
+    testing::StrictMock<test::SysMock> mock;
+    const int ifd = 1;
+    EXPECT_CALL(mock, dup(1)).WillOnce(Return(2));
+    DupableFd fd(ifd, &mock);
+    EXPECT_EQ(2, fd.getVal());
+    EXPECT_CALL(mock, close(2)).WillOnce(Return(0));
+}
+
+TEST(DupableFd, MoveConstruct)
+{
+    testing::StrictMock<test::SysMock> mock;
+    DupableFd fd(1, &mock);
+    DupableFd fd2(std::move(fd));
+    EXPECT_CALL(mock, close(1)).WillOnce(Return(0));
+}
+
+TEST(DupableFd, MoveAssign)
+{
+    testing::StrictMock<test::SysMock> mock1, mock2;
+    DupableFd fd2(2, &mock2);
+    {
+        DupableFd fd(1, &mock1);
+        EXPECT_CALL(mock2, close(2)).WillOnce(Return(0));
+        fd2 = std::move(fd);
+    }
+    EXPECT_CALL(mock1, close(1)).WillOnce(Return(0));
+}
+
+TEST(DupableFd, CopyConstruct)
+{
+    testing::StrictMock<test::SysMock> mock;
+    DupableFd fd(1, &mock);
+    EXPECT_CALL(mock, dup(1)).WillOnce(Return(2));
+    DupableFd fd2(fd);
+    EXPECT_CALL(mock, close(2)).WillOnce(Return(0));
+    EXPECT_CALL(mock, close(1)).WillOnce(Return(0));
+}
+
+TEST(DupableFd, CopyAssign)
+{
+    testing::StrictMock<test::SysMock> mock1, mock2;
+    DupableFd fd2(2, &mock2);
+    {
+        DupableFd fd(1, &mock1);
+        EXPECT_CALL(mock2, close(2)).WillOnce(Return(0));
+        EXPECT_CALL(mock1, dup(1)).WillOnce(Return(3));
+        fd2 = fd;
+        EXPECT_CALL(mock1, close(1)).WillOnce(Return(0));
+    }
+    EXPECT_CALL(mock1, close(3)).WillOnce(Return(0));
+}
+
+} // namespace
+} // namespace fd
+} // namespace stdplus
