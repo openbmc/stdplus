de320a2977836d95c01af0b19cb20db588f7ea5f
William A. Kennington III <wak@google.com>
2019-04-27 14:06:33 -0700

    future/util: Add method to check multiple future readiness
    In order to support callbacks that contain multiple futures, we need to
    know if their futures are ready. This allows us to check any number of
    static or dynamic futures that exist as arguments to a function.
    
    Change-Id: I8e54de1e943870ecf5acc7637414a8a594478a48
    Signed-off-by: William A. Kennington III <wak@google.com>

---
+++ src/stdplus/future/util.hpp
@@ -0,0 +1,67 @@
+#pragma once
+#include <chrono>
+#include <future>
+#include <tuple>
+
+namespace stdplus
+{
+namespace future
+{
+
+/** @brief Determines if a future is ready to be used without blocking
+ *
+ *  @param[in] future - The future we are checking
+ *  @return True if ready to read
+ */
+template <typename T>
+bool isReady(const std::future<T>& future)
+{
+    // TODO(wak): Replace wait_for() with is_ready() when stabilized
+    using namespace std::chrono_literals;
+    return future.wait_for(0s) == std::future_status::ready;
+}
+
+/** @brief Determines if all futures in an iterable container are ready
+ *         to be used without blocking
+ *
+ *  @param[in] futures - The futures we are checking
+ *  @return True if all are ready to read
+ */
+template <typename C, typename = typename C::const_iterator>
+bool isReady(const C& futures)
+{
+    for (const auto& future : futures)
+    {
+        if (!isReady(future))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+/** @brief Determines if all futures are ready to be used without blocking
+ *
+ *  @param[in] futures... - The futures we are checking
+ *  @return True if all are ready to read
+ */
+template <typename... Futures>
+bool allReady(const Futures&... futures)
+{
+    return (... && isReady(futures));
+}
+
+/** @brief Determines if all futures in a tuple are ready to be used
+ *         without blocking
+ *
+ *  @param[in] futures - The futures we are checking
+ *  @return True if all are ready to read
+ */
+template <typename... Futures>
+bool isReady(const std::tuple<Futures...>& futures)
+{
+    return std::apply(allReady<Futures...>, futures);
+}
+
+} // namespace future
+} // namespace stdplus
