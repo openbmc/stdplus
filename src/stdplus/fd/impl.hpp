de627674797ac0e9c63eecf56dc5464729352352
William A. Kennington III <wak@google.com>
2020-08-03 13:57:14 -0700

    fd: Implement managed file descriptor support
    Change-Id: I0c5c438aa2c31ae52e115951b3fb1e85df182fd1
    Signed-off-by: William A. Kennington III <wak@google.com>

---
+++ src/stdplus/fd/impl.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <stdplus/fd/intf.hpp>
+
+namespace stdplus
+{
+
+class FdImpl : public Fd
+{
+  public:
+    virtual int get() const = 0;
+
+    span<std::byte> read(span<std::byte> buf) override;
+    span<std::byte> recv(span<std::byte> buf, int flags) override;
+    span<const std::byte> write(span<const std::byte> data) override;
+    span<const std::byte> send(span<const std::byte> data, int flags) override;
+    size_t lseek(off_t offset, int whence) override;
+    void truncate(off_t size) override;
+    void bind(span<const std::byte> sockaddr) override;
+    void setsockopt(int level, int optname, span<const std::byte> opt) override;
+    int ioctl(unsigned long id, void* data) override;
+    int constIoctl(unsigned long id, void* data) const override;
+    void fcntlSetfd(int flags) override;
+    int fcntlGetfd() const override;
+    void fcntlSetfl(int flags) override;
+    int fcntlGetfl() const override;
+};
+
+} // namespace stdplus
---
+++ test/fd/managed.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+#include <stdplus/fd/managed.hpp>
+
+namespace stdplus
+{
+namespace fd
+{
+
+TEST(DupableFd, Noop)
+{
+}
+
+} // namespace fd
+} // namespace stdplus
