a3110f292b13704437ed79d5c3cfe77219c394a3
William A. Kennington III <wak@google.com>
2020-08-03 13:57:14 -0700

    fd: Implement managed file descriptor support
    Change-Id: I0c5c438aa2c31ae52e115951b3fb1e85df182fd1
    Signed-off-by: William A. Kennington III <wak@google.com>

---
+++ src/stdplus/fd/intf.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <cstddef>
+#include <stdplus/types.hpp>
+
+namespace stdplus
+{
+
+class Fd
+{
+  public:
+    virtual ~Fd() = default;
+
+    virtual span<std::byte> read(span<std::byte> buf) = 0;
+    virtual span<std::byte> recv(span<std::byte> buf, int flags) = 0;
+    virtual span<const std::byte> write(span<const std::byte> data) = 0;
+    virtual span<const std::byte> send(span<const std::byte> data,
+                                       int flags) = 0;
+    virtual size_t lseek(off_t offset, int whence) = 0;
+    virtual void truncate(off_t size) = 0;
+    virtual void bind(span<const std::byte> sockaddr) = 0;
+    virtual void setsockopt(int level, int optname,
+                            span<const std::byte> opt) = 0;
+    virtual int ioctl(unsigned long id, void* data) = 0;
+    virtual void fcntl_setfl(int flags) = 0;
+};
+
+} // namespace stdplus
---
+++ src/stdplus/fd/ops.cpp
@@ -0,0 +1,92 @@
+#include <stdplus/exception.hpp>
+#include <stdplus/fd/ops.hpp>
+#include <utility>
+
+namespace stdplus
+{
+namespace fd
+{
+namespace detail
+{
+
+template <typename Fun, typename Byte, typename... Args>
+static void opAll(const char* name, Fun&& fun, Fd& fd, span<Byte> data,
+                  Args&&... args)
+{
+    while (data.size() > 0)
+    {
+        auto ret = (fd.*fun)(data, std::forward<Args>(args)...);
+        if (ret.size() == 0)
+        {
+            throw exception::WouldBlock(
+                fmt::format("{} missing {}B", name, data.size()));
+        }
+        data = data.subspan(ret.size());
+    }
+}
+
+void readAll(Fd& fd, span<std::byte> data)
+{
+    opAll("readAll", &Fd::read, fd, data);
+}
+
+void recvAll(Fd& fd, span<std::byte> data, int flags)
+{
+    opAll("recvAll", &Fd::recv, fd, data, flags);
+}
+
+void writeAll(Fd& fd, span<const std::byte> data)
+{
+    opAll("writeAll", &Fd::write, fd, data);
+}
+
+void sendAll(Fd& fd, span<const std::byte> data, int flags)
+{
+    opAll("sendAll", &Fd::send, fd, data, flags);
+}
+
+template <typename Fun, typename Byte, typename... Args>
+static span<Byte> opAligned(const char* name, Fun&& fun, Fd& fd, size_t align,
+                            span<Byte> data, Args&&... args)
+{
+    span<Byte> ret;
+    do
+    {
+        auto r =
+            (fd.*fun)(data.subspan(ret.size()), std::forward<Args>(args)...);
+        if (ret.size() != 0 && r.size() == 0)
+        {
+            throw exception::WouldBlock(
+                fmt::format("{} is {}B/{}B", name, ret.size() % align, align));
+        }
+        ret = data.subspan(0, ret.size() + r.size());
+    } while (ret.size() % align != 0);
+    return ret;
+}
+
+span<std::byte> readAligned(Fd& fd, size_t align, span<std::byte> buf)
+{
+    return opAligned("readAligned", &Fd::read, fd, align, buf);
+}
+
+span<std::byte> recvAligned(Fd& fd, size_t align, span<std::byte> buf,
+                            int flags)
+{
+    return opAligned("recvAligned", &Fd::recv, fd, align, buf, flags);
+}
+
+span<const std::byte> writeAligned(Fd& fd, size_t align,
+                                   span<const std::byte> data)
+{
+    return opAligned("writeAligned", &Fd::write, fd, align, data);
+}
+
+span<const std::byte> sendAligned(Fd& fd, size_t align,
+                                  span<const std::byte> data, int flags)
+{
+    return opAligned("sendAligned", &Fd::send, fd, align, data, flags);
+}
+
+} // namespace detail
+} // namespace fd
+} // namespace stdplus
