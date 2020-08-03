a05b91e14060c530c865cb806bb436ce07359266
William A. Kennington III <wak@google.com>
2020-08-03 13:57:14 -0700

    fd: Implement managed file descriptor support
    Change-Id: I0c5c438aa2c31ae52e115951b3fb1e85df182fd1
    Signed-off-by: William A. Kennington III <wak@google.com>

---
+++ src/stdplus/fd/dupable.cpp
@@ -0,0 +1,38 @@
+#include <fcntl.h>
+#include <stdplus/fd/dupable.hpp>
+#include <stdplus/fd/ops.hpp>
+#include <stdplus/util/cexec.hpp>
+#include <utility>
+
+namespace stdplus
+{
+namespace detail
+{
+
+int ref(const int& fd)
+{
+    return CHECK_ERRNO(fcntl(fd, F_DUPFD_CLOEXEC, fd), "fcntl dupfd_cloexec");
+}
+
+} // namespace detail
+
+DupableFd::DupableFd(const int& fd) : handle(fd)
+{
+}
+
+DupableFd::DupableFd(int&& fd) : handle(std::move(fd))
+{
+    fd::setFdFlags(*this, fd::getFdFlags(*this).set(fd::FdFlag::CloseOnExec));
+}
+
+int DupableFd::release()
+{
+    return handle.release();
+}
+
+int DupableFd::get()
+{
+    return handle.value();
+}
+
+} // namespace stdplus
