12636ed7bc2ac894fdccb23e29f3b8877e0a1512
William A. Kennington III <wak@google.com>
2020-08-03 13:57:14 -0700

    fd: Implement managed file descriptor support
    Change-Id: I0c5c438aa2c31ae52e115951b3fb1e85df182fd1
    Signed-off-by: William A. Kennington III <wak@google.com>

---
+++ src/stdplus/fd/gmock.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <gmock/gmock.h>
+#include <stdplus/fd/intf.hpp>
+
+namespace stdplus
+{
+
+class FdMock : public Fd
+{
+  public:
+    MOCK_METHOD(span<std::byte>, read, (span<std::byte> buf), (override));
+    MOCK_METHOD(span<std::byte>, recv, (span<std::byte> buf, int flags),
+                (override));
+    MOCK_METHOD(span<const std::byte>, write, (span<const std::byte> data),
+                (override));
+    MOCK_METHOD(span<const std::byte>, send,
+                (span<const std::byte> data, int flags), (override));
+    MOCK_METHOD(size_t, lseek, (off_t offset, int whence), (override));
+    MOCK_METHOD(void, truncate, (off_t size), (override));
+    MOCK_METHOD(void, bind, (span<const std::byte> sockaddr), (override));
+    MOCK_METHOD(void, setsockopt,
+                (int level, int optname, span<const std::byte> opt),
+                (override));
+    MOCK_METHOD(int, ioctl, (unsigned long id, void* data), (override));
+    MOCK_METHOD(int, constIoctl, (unsigned long id, void* data),
+                (const, override));
+    MOCK_METHOD(void, fcntlSetfd, (int flags), (override));
+    MOCK_METHOD(int, fcntlGetfd, (), (const, override));
+    MOCK_METHOD(void, fcntlSetfl, (int flags), (override));
+    MOCK_METHOD(int, fcntlGetfl, (), (const, override));
+};
+
+} // namespace stdplus
