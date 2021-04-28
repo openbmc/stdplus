916be89dcb94b38706bfade69a1d708554ed46f3
William A. Kennington III <wak@google.com>
2021-04-28 12:52:43 -0700

    dl: Add support for dlopen
    Change-Id: I3fd109860a1eb0a945ddc3a0376ade248a4b2f75
    Signed-off-by: William A. Kennington III <wak@google.com>

---
+++ src/stdplus/dl.cpp
@@ -0,0 +1,45 @@
+#include <dlfcn.h>
+#include <fmt/format.h>
+
+#include <stdplus/dl.hpp>
+
+namespace stdplus
+{
+
+Dl::Dl(const char* file, DlOpenFlags flags) :
+    handle(open(file, static_cast<int>(flags)))
+{
+}
+
+struct link_map* Dl::linkMap()
+{
+    struct link_map* ret;
+    info(RTLD_DI_LINKMAP, &ret);
+    return ret;
+}
+
+void Dl::info(int request, void* info)
+{
+    if (::dlinfo(*handle, request, info) != 0)
+    {
+        throw std::runtime_error("dlinfo");
+    };
+}
+
+void* Dl::open(const char* file, int flags)
+{
+    void* ret = ::dlopen(file, flags);
+    if (ret == nullptr)
+    {
+        throw std::runtime_error(fmt::format(
+            "dlopen `{}`: {}", file ? file : "<nullptr>", dlerror()));
+    }
+    return ret;
+}
+
+void Dl::close(void*&& handle)
+{
+    ::dlclose(handle);
+}
+
+} // namespace stdplus
---
+++ src/stdplus/dl.hpp
@@ -0,0 +1,55 @@
+#pragma once
+#include <dlfcn.h>
+#include <link.h>
+
+#include <stdplus/flags.hpp>
+#include <stdplus/handle/managed.hpp>
+
+namespace stdplus
+{
+
+enum class DlOpenType : int
+{
+    Lazy = RTLD_LAZY,
+    Now = RTLD_NOW,
+};
+
+enum class DlOpenFlag : int
+{
+    Global = RTLD_GLOBAL,
+    Local = RTLD_LOCAL,
+    NoDelete = RTLD_NODELETE,
+    NoLoad = RTLD_NOLOAD,
+    DeepBind = RTLD_DEEPBIND,
+};
+
+class DlOpenFlags : public stdplus::BitFlags<int, DlOpenFlag>
+{
+  public:
+    inline DlOpenFlags(DlOpenType type) :
+        BitFlags<int, DlOpenFlag>(static_cast<int>(type))
+    {
+    }
+
+    inline DlOpenFlags(BitFlags<int, DlOpenFlag> flags) :
+        BitFlags<int, DlOpenFlag>(flags)
+    {
+    }
+};
+
+class Dl
+{
+  public:
+    Dl(const char* filename, DlOpenFlags flags);
+
+    struct link_map* linkMap();
+
+  private:
+    void info(int request, void* info);
+
+    static void* open(const char* filename, int flags);
+    static void close(void*&& handle);
+    stdplus::Managed<void*>::Handle<close> handle;
+};
+
+} // namespace stdplus
