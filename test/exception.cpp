9a70f4e1a5d0ca4380d3c44e4ebcf6c905d73531
William A. Kennington III <wak@google.com>
2021-05-01 17:16:57 -0700

    exception: Add wrapper to ignore lambda failures
    Change-Id: I2bd720775b233a29083ac5b572bdec1671f6c634
    Signed-off-by: William A. Kennington III <wak@google.com>

---
+++ test/exception.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include <stdplus/exception.hpp>
+
+namespace stdplus
+{
+namespace exception
+{
+
+TEST(Exception, IgnoreNoError)
+{
+    ignore([] {})();
+    ignore([]() mutable { throw std::runtime_error("Boom"); })();
+    EXPECT_EQ(int(), ignore([]() -> int { throw 1; })());
+    auto x = std::make_unique<int>(1);
+    auto y = std::make_unique<int>(2);
+    auto z = std::make_unique<int>(3);
+    EXPECT_EQ(3, ignore([x = std::move(x)](auto&& v) { return *v + *x; })(y));
+    EXPECT_EQ(5, ignore([z = std::move(z)](auto v) { return *v + *z; })(
+                     std::move(y)));
+}
+
+} // namespace exception
+} // namespace stdplus
