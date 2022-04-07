4bbdd90294e3d02ea29d0b506004ca31b9708e28
Willy Tu <wltu@google.com>
2022-04-07 11:02:07 -0700

    use std::span instead of nonstd::span
    Signed-off-by: Willy Tu <wltu@google.com>
    Change-Id: Icb6cc5e9ecb6ba8a7ec181871fa6da9ef7620abb

--- src/stdplus/types.hpp
+++ src/stdplus/types.hpp
@@ -1,16 +1,11 @@
 #pragma once
-#if __has_include(<nonstd/span.hpp>)
-#define span_CONFIG_SELECT_SPAN span_SPAN_NONSTD
-#include <nonstd/span.hpp>
-#define STDPLUS_SPAN_TYPE nonstd::span
-#endif
+#include <span>
+#define STDPLUS_SPAN_TYPE std::span
 
 namespace stdplus
 {
 
-#ifdef STDPLUS_SPAN_TYPE
 template <typename... Args>
 using span = STDPLUS_SPAN_TYPE<Args...>;
-#endif
 
 } // namespace stdplus
