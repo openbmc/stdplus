16839f4d1d3822d20087b35961211cede8c04a7b
William A. Kennington III <wak@google.com>
2022-08-12 17:39:44 -0700

    fmt/format: Add storage consistent formatted output
    This makes it trivial for a caller to write to files piecewise while
    still guaranteeing that the output is always consistent. It performs
    buffered writes to a tmpfile and only once successful does it swap out
    for the resulting file.
    
    Change-Id: I7e733a283ee60a47ddc6923cd9579fa49a7c5434
    Signed-off-by: William A. Kennington III <wak@google.com>

--- include-fd/stdplus/fd/fmt.hpp
+++ include-fd/stdplus/fd/fmt.hpp
@@ -1,7 +1,9 @@
 #pragma once
+#include <filesystem>
 #include <fmt/format.h>
 #include <functional>
 #include <stdplus/fd/intf.hpp>
+#include <stdplus/fd/managed.hpp>
 
 namespace stdplus
 {
@@ -34,6 +36,35 @@ class FormatBuffer
     size_t max;
 
     void writeIfNeeded();
+};
+
+class FormatToFile
+{
+  public:
+    explicit FormatToFile(const char* tmpl = "/tmp/stdplus.XXXXXX");
+    ~FormatToFile();
+    FormatToFile(const FormatToFile&) = delete;
+    FormatToFile(FormatToFile&&) = delete;
+    FormatToFile& operator=(const FormatToFile&) = delete;
+    FormatToFile& operator=(FormatToFile&&) = delete;
+
+    template <typename... Args>
+    void append(fmt::format_string<Args...> fmt, Args&&... args)
+    {
+        buf.append(fmt, std::forward<Args>(args)...);
+    }
+
+    void commit(const std::filesystem::path& out, int mode = 0644);
+
+    inline const std::string& getTmpname() const
+    {
+        return tmpname;
+    }
+
+  private:
+    std::string tmpname;
+    stdplus::ManagedFd fd;
+    FormatBuffer buf;
 };
 
 } // namespace fd
