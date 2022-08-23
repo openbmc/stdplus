a8176b814d6d499ea298854bbd16a2f60d065496
William A. Kennington III <wak@google.com>
2022-08-23 16:35:10 -0700

    fd/atomic: Better separate this orthogonal functionality
    Nothing is using the old interface, so we can move it for better
    separation of responisbilities.
    
    Change-Id: I3b6e429b106aa22e58df25e0801b60af0fbd6d70
    Signed-off-by: William A. Kennington III <wak@google.com>

---
+++ include-fd/stdplus/fd/atomic.hpp
@@ -0,0 +1,40 @@
+#pragma once
+#include <filesystem>
+#include <stdplus/fd/managed.hpp>
+#include <string>
+#include <string_view>
+
+namespace stdplus
+{
+namespace fd
+{
+
+class AtomicWriter : public stdplus::FdImpl
+{
+  public:
+    AtomicWriter(const std::filesystem::path& filename, int mode,
+                 std::string_view tmpl = {});
+    AtomicWriter(AtomicWriter&& other);
+    AtomicWriter& operator=(AtomicWriter&& other);
+    ~AtomicWriter();
+
+    void commit(bool allow_copy = false);
+
+    inline const std::string& getTmpname() const
+    {
+        return tmpname;
+    }
+
+    int get() const override;
+
+  private:
+    std::filesystem::path filename;
+    int mode;
+    std::string tmpname;
+    ManagedFd fd;
+
+    void cleanup() noexcept;
+};
+
+} // namespace fd
+} // namespace stdplus
