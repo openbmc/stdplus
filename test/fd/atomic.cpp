765063f75d1bc998f7387cf35f590fed9d0258d9
William A. Kennington III <wak@google.com>
2022-08-24 14:30:38 -0700

    fd/atomic: Support O_TMPFILE if possible
    We want to make the tempfile as crash resistant as possible. Marking it
    as O_TMPFILE means the OS will clean up the file if anything crashes
    outside typical exception control flow.
    
    Change-Id: Ie52268c043026c083f4aa749334b3153724e1a41
    Signed-off-by: William A. Kennington III <wak@google.com>

--- test/fd/atomic.cpp
+++ test/fd/atomic.cpp
@@ -38,6 +38,20 @@ class AtomicWriterTest : public gtest::TestWithTmp
 
 TEST_F(AtomicWriterTest, NoCommit)
 {
+    detail::tmpfile_support = true;
+    ASSERT_NO_THROW(file = std::make_unique<AtomicWriter>(filename, 0644));
+    writeExact(*file, "hi\n"sv);
+    EXPECT_FALSE(std::filesystem::exists(filename));
+    const auto& tmpname = file->getTmpname();
+    if (!tmpname.empty())
+    {
+        EXPECT_TRUE(std::filesystem::exists(tmpname));
+    }
+}
+
+TEST_F(AtomicWriterTest, NoCommitNoTmpfile)
+{
+    detail::tmpfile_support = false;
     ASSERT_NO_THROW(file = std::make_unique<AtomicWriter>(filename, 0644));
     writeExact(*file, "hi\n"sv);
     EXPECT_FALSE(std::filesystem::exists(filename));
@@ -46,6 +60,18 @@ TEST_F(AtomicWriterTest, NoCommit)
 
 TEST_F(AtomicWriterTest, BadCommit)
 {
+    detail::tmpfile_support = true;
+    auto tmp = fmt::format("{}/tmp.XXXXXX", CaseTmpDir());
+    ASSERT_NO_THROW(file =
+                        std::make_unique<AtomicWriter>("/dev/null", 0644, tmp));
+    writeExact(*file, "hi\n"sv);
+    EXPECT_THROW(file->commit(), std::filesystem::filesystem_error);
+    EXPECT_EQ(file->getTmpname(), ""sv);
+}
+
+TEST_F(AtomicWriterTest, BadCommitNoTmpfile)
+{
+    detail::tmpfile_support = false;
     auto tmp = fmt::format("{}/tmp.XXXXXX", CaseTmpDir());
     ASSERT_NO_THROW(file =
                         std::make_unique<AtomicWriter>("/dev/null", 0644, tmp));
@@ -57,6 +83,19 @@ TEST_F(AtomicWriterTest, BadCommit)
 
 TEST_F(AtomicWriterTest, Basic)
 {
+    detail::tmpfile_support = true;
+    ASSERT_NO_THROW(file = std::make_unique<AtomicWriter>(filename, 0644));
+    writeExact(*file, "hi\n"sv);
+    EXPECT_NO_THROW(file->commit());
+    EXPECT_EQ(file->getTmpname(), ""sv);
+    std::error_code ec;
+    EXPECT_EQ(3, std::filesystem::file_size(filename, ec));
+    EXPECT_EQ(std::errc{}, ec);
+}
+
+TEST_F(AtomicWriterTest, BasicNoTmpfile)
+{
+    detail::tmpfile_support = false;
     ASSERT_NO_THROW(file = std::make_unique<AtomicWriter>(filename, 0644));
     writeExact(*file, "hi\n"sv);
     EXPECT_TRUE(std::filesystem::exists(file->getTmpname()));
