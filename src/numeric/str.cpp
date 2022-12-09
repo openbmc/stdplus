f901498fc97869d49951d294f717778c6414b61f
William A. Kennington III <wak@google.com>
2022-12-09 02:17:22 -0800

    numeric/str: Add constexpr int decode
    This makes it possible to convert strings to numbers in a constexpr
    context.
    
    Change-Id: I051ce8600b643533cc25a8de06e15c917db9fbd7
    Signed-off-by: William A. Kennington III <wak@google.com>

--- src/numeric/str.cpp
+++ src/numeric/str.cpp
@@ -4,4 +4,6 @@ namespace stdplus::detail
 {
 template char* uintToStr<16>(char*, uintptr_t, uint8_t) noexcept;
 template char* uintToStr<10>(char*, uintptr_t, uint8_t) noexcept;
+template uintptr_t strToUInt<16>(std::string_view) noexcept;
+template uintptr_t strToUInt<10>(std::string_view) noexcept;
 } // namespace stdplus::detail
