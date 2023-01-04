7a7c23fe28f323e2463081b0377d730199233b69
William A. Kennington III <wak@google.com>
2023-01-03 18:15:47 -0800

    hash: Add support for arrays
    There is now builtin support for stdplus::hash<> and
    stdplus::hashMulti(...) hashing std::array<> and C array containers.
    
    Change-Id: I111511fcd2c1627011b47cf3593cc9d63d752c1a
    Signed-off-by: William A. Kennington III <wak@google.com>

--- test/hash.cpp
+++ test/hash.cpp
@@ -1,5 +1,6 @@
 #include <stdplus/hash.hpp>
 
+#include <array>
 #include <string>
 #include <tuple>
 
@@ -12,8 +13,10 @@ TEST(HashMulti, Basic)
 {
     EXPECT_EQ(0, hashMulti());
     EXPECT_EQ(2654435834, hashMulti(1, 2));
+    int s[4] = {1, 2, 3, 4};
+    std::array<uint16_t, 2> a = {3, 4};
     std::tuple<std::string, int> t = {"s", 8};
-    hashMulti(1, std::string("bacon"), nullptr, t);
+    hashMulti(1, std::string("bacon"), nullptr, s, a, t);
 }
 
 } // namespace stdplus
