322e869da4e3bb43614e7caa7e824195809829c2
William A. Kennington III <wak@google.com>
2023-06-14 13:26:47 -0700

    net/addr/ether: Add FromStr conversion
    Change-Id: I85ae2b914fa5a84d2acb103a61d78ce60ab9bef4
    Signed-off-by: William A. Kennington III <wak@google.com>

--- include/stdplus/net/addr/ether.hpp
+++ include/stdplus/net/addr/ether.hpp
@@ -2,8 +2,11 @@
 #include <net/ethernet.h>
 
 #include <stdplus/hash.hpp>
+#include <stdplus/numeric/str.hpp>
+#include <stdplus/str/conv.hpp>
 
 #include <algorithm>
+#include <stdexcept>
 #include <variant>
 
 namespace stdplus
@@ -23,6 +26,39 @@ struct EtherAddr : ether_addr
     {
         return std::equal(ether_addr_octet, ether_addr_octet + 6,
                           rhs.ether_addr_octet);
+    }
+};
+
+template <>
+struct FromStr<EtherAddr>
+{
+    template <typename CharT>
+    constexpr EtherAddr operator()(std::basic_string_view<CharT> sv) const
+    {
+        constexpr StrToInt<16, std::uint8_t> sti;
+        EtherAddr ret;
+        if (sv.size() == 12 && sv.find(":") == sv.npos)
+        {
+            for (size_t i = 0; i < 6; ++i)
+            {
+                ret.ether_addr_octet[i] = sti(sv.substr(i * 2, 2));
+            }
+        }
+        else
+        {
+            for (size_t i = 0; i < 5; ++i)
+            {
+                auto loc = sv.find(":");
+                ret.ether_addr_octet[i] = sti(sv.substr(0, loc));
+                sv.remove_prefix(loc == sv.npos ? sv.size() : loc + 1);
+                if (sv.empty())
+                {
+                    throw std::invalid_argument("Missing mac data");
+                }
+            }
+            ret.ether_addr_octet[5] = sti(sv);
+        }
+        return ret;
     }
 };
 
