7c9365c81db2dabc4297d228fb908e146ed03a89
William A. Kennington III <wak@google.com>
2023-06-14 17:41:13 -0700

    net/addr/ip: Add In4Addr ToStr conversion
    Change-Id: Id2f16834c02a2bc500aae45dc852113cee1eff87
    Signed-off-by: William A. Kennington III <wak@google.com>

--- src/net/addr/ip.cpp
+++ src/net/addr/ip.cpp
@@ -3,4 +3,5 @@
 namespace stdplus
 {
 template In4Addr FromStr<In4Addr>::operator()(std::string_view) const;
-}
+template char* ToStr<In4Addr>::operator()(char*, In4Addr) const noexcept;
+} // namespace stdplus
