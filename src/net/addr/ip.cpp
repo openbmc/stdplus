4167cefea1db19c533986f1552bda6b348a1b3c1
William A. Kennington III <wak@google.com>
2023-06-16 16:05:52 -0700

    net/addr/ip: Add IPv6 ToStr conversion
    Change-Id: Icab45bc29732eb9dc6cf42532ec74fe1c48afa91
    Signed-off-by: William A. Kennington III <wak@google.com>

--- src/net/addr/ip.cpp
+++ src/net/addr/ip.cpp
@@ -5,4 +5,5 @@ namespace stdplus
 template In4Addr FromStr<In4Addr>::operator()(std::string_view) const;
 template char* ToStr<In4Addr>::operator()(char*, In4Addr) const noexcept;
 template In6Addr FromStr<In6Addr>::operator()(std::string_view) const;
+template char* ToStr<In6Addr>::operator()(char*, In6Addr) const noexcept;
 } // namespace stdplus
