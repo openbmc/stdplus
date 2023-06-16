66531cb110e1328486317fa87e9e503d8e4fefa7
William A. Kennington III <wak@google.com>
2023-06-16 16:17:35 -0700

    net/addr/ip: Add InAnyAddr ToStr conversion
    Change-Id: I664d53c15df5ab9f6bf9b9e0efa653d5b726896b
    Signed-off-by: William A. Kennington III <wak@google.com>

--- src/net/addr/ip.cpp
+++ src/net/addr/ip.cpp
@@ -7,4 +7,5 @@ template char* ToStr<In4Addr>::operator()(char*, In4Addr) const noexcept;
 template In6Addr FromStr<In6Addr>::operator()(std::string_view) const;
 template char* ToStr<In6Addr>::operator()(char*, In6Addr) const noexcept;
 template InAnyAddr FromStr<InAnyAddr>::operator()(std::string_view) const;
+template char* ToStr<InAnyAddr>::operator()(char*, InAnyAddr) const noexcept;
 } // namespace stdplus
