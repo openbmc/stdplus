d5957f5b303c6311c120824c4dd0bfc4f01bd340
William A. Kennington III <wak@google.com>
2023-06-16 16:55:01 -0700

    net/addr/subnet: Add ToStr conversion
    Change-Id: Id06045fd638fa53eb5725eadd63cace148fc53f3
    Signed-off-by: William A. Kennington III <wak@google.com>

--- src/net/addr/subnet.cpp
+++ src/net/addr/subnet.cpp
@@ -15,4 +15,9 @@ void invalidSubnetPfx(std::size_t pfx)
 template class Subnet46<In4Addr, uint8_t>;
 template class Subnet46<In6Addr, uint8_t>;
 
+template char* SubnetToStr<Subnet4>::operator()(char*, Subnet4) const noexcept;
+template char* SubnetToStr<Subnet6>::operator()(char*, Subnet6) const noexcept;
+template char* SubnetToStr<SubnetAny>::operator()(char*,
+                                                  SubnetAny) const noexcept;
+
 } // namespace stdplus::detail
