4d01d8a58c2afe6a4287a31451b5875859724b6a
William A. Kennington III <wak@google.com>
2023-06-16 17:09:43 -0700

    net/addr/subnet: Add FromStr conversion
    Change-Id: I41ef5adda1e8784e10259989bea3316b94668b9b
    Signed-off-by: William A. Kennington III <wak@google.com>

--- src/net/addr/subnet.cpp
+++ src/net/addr/subnet.cpp
@@ -15,6 +15,10 @@ void invalidSubnetPfx(std::size_t pfx)
 template class Subnet46<In4Addr, uint8_t>;
 template class Subnet46<In6Addr, uint8_t>;
 
+template Subnet4 SubnetFromStr<Subnet4>::operator()(std::string_view) const;
+template Subnet6 SubnetFromStr<Subnet6>::operator()(std::string_view) const;
+template SubnetAny SubnetFromStr<SubnetAny>::operator()(std::string_view) const;
+
 template char* SubnetToStr<Subnet4>::operator()(char*, Subnet4) const noexcept;
 template char* SubnetToStr<Subnet6>::operator()(char*, Subnet6) const noexcept;
 template char* SubnetToStr<SubnetAny>::operator()(char*,
