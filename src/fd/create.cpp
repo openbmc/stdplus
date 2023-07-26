e6fda4501899d9d886f9d5c035f4cf759b459861
William A. Kennington III <wak@google.com>
2023-07-26 15:12:12 -0700

    fd: Use zstrings
    Change-Id: I626c269f0a4ab5130e21acbabf0c62cf3c6a3aec
    Signed-off-by: William A. Kennington III <wak@google.com>

--- src/fd/create.cpp
+++ src/fd/create.cpp
@@ -11,11 +11,11 @@ namespace stdplus
 namespace fd
 {
 
-DupableFd open(const char* pathname, OpenFlags flags, mode_t mode)
+DupableFd open(const_zstring pathname, OpenFlags flags, mode_t mode)
 {
     return DupableFd(
-        CHECK_ERRNO(::open(pathname, static_cast<int>(flags), mode),
-                    std::format("open `{}`", pathname)));
+        CHECK_ERRNO(::open(pathname.c_str(), static_cast<int>(flags), mode),
+                    std::format("open `{}`", pathname.c_str())));
 }
 
 DupableFd socket(SocketDomain domain, SocketType type, SocketProto protocol)
